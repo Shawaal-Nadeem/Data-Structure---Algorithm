2b340d698e01ef4860c5677f3950ede43427c535
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-05-14 14:02:36 +0500

    Update Source.cpp

--- Doubly Circular Linked List/Doubly Circular Linked List/Source.cpp
+++ Doubly Circular Linked List/Doubly Circular Linked List/Source.cpp
@@ -8,11 +8,6 @@ int main()
 	obj.insertatLast(3);
 	obj.insertatLast(4);
 	obj.insertatLast(5);
-	obj.deleteatFirst();
-	obj.deleteatFirst();
-	obj.deleteatFirst();
-	obj.deleteatFirst();
-	obj.deleteatFirst();
 
 	obj.display();
 	return 0;
