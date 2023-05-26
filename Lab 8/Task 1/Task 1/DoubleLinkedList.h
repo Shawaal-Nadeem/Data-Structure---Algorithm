fda046b53943480621f3d5c682277e7490de64a2
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-05-26 14:45:21 +0500

    Lab 8 Task 1 complete

---
+++ Lab 8/Task 1/Task 1/DoubleLinkedList.h
@@ -0,0 +1,63 @@
+#include"Node.h"
+
+class DoubleLinkedList
+{
+	Node*head;
+	Node*tail;
+public:
+	DoubleLinkedList()
+	{
+		head = nullptr;
+		tail = nullptr;
+	}
+	void insert(int val)
+	{
+		Node*temp = new Node;
+		temp->data = val;
+		if (head == nullptr && tail == nullptr)
+		{
+			temp->next = nullptr;
+			temp->prev = nullptr;
+			head = temp;
+			tail = temp;
+		}
+		else
+		{
+			temp->next = nullptr;
+			temp->prev = tail;
+			tail->next = temp;
+			tail = temp;
+		}
+	}
+	int sum(Node*temp)
+	{
+		if (head->next == nullptr && tail->next == nullptr)
+		{
+			return head->data;
+		}
+		
+		else
+		{
+			int x = 0;
+			 if (temp == tail)
+			{
+				 return x = x + temp->data;
+			}
+		    x = temp->data;
+			 temp = temp->next;
+			x = x + sum(temp);
+			return x;
+		}
+	}
+	void display()
+	{
+		Node*temp = head;
+		while (temp != nullptr)
+		{
+			cout << temp->data << "->";
+			temp = temp->next;
+		}
+		cout << endl;
+		cout << "Sum of List are : " << sum(head) << endl;
+	}
+};
---
+++ Lab 8/Task 1/Task 1/Source.cpp
@@ -0,0 +1,13 @@
+#include"DoubleLinkedList.h"
+
+int main()
+{
+	DoubleLinkedList obj;
+	obj.insert(1);
+	obj.insert(2);
+	obj.insert(3);
+	obj.insert(4);
+	obj.insert(20);
+	obj.display();
+	return 0;
+}
