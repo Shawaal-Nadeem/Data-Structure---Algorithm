7a74f2070b08eb2dfb4081165a866038e993348d
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-06-20 14:37:53 +0500

    Task 1 completed

---
+++ Lab 11/Task 1/Task 1/HeapTree.h
@@ -0,0 +1,100 @@
+#include<iostream>
+using namespace std;
+
+class HeapTree
+{
+	int *heapArr;
+	int size;
+	int countNodes;
+public:
+	HeapTree(int s)
+	{
+		size = s;
+		heapArr = new int[size];
+		countNodes = 0;
+	}
+	int parent(int index)
+	{
+		int parentIndex = (index - 1) / 2;
+		return parentIndex;
+	}
+	void swap(int &parentVal, int &childVal)
+	{
+		int temp = parentVal;
+		parentVal = childVal;
+		childVal = temp;
+	}
+	void insert(int value)
+	{
+		if (countNodes == size)
+		{
+			cout << "Heap Tree is Full" << endl;
+		}
+		else
+		{
+			heapArr[countNodes] = value;
+			countNodes++;
+		}
+	}
+	void min_Heap()
+	{
+		int index = size - 1;
+		while (index > 0)
+		{
+			if (heapArr[parent(index)] > heapArr[index])
+			{
+				int parentVal = heapArr[parent(index)];
+				int childVal = heapArr[index];
+				swap(parentVal, childVal);
+				heapArr[parent(index)] = parentVal;
+				heapArr[index] = childVal;
+				index = parent(index);
+			}
+			else
+			{
+				index = parent(index);
+			}
+		}
+		cout << "Min Heap : ";
+		for (int i = 0; i < size; i++)
+		{
+			cout << heapArr[i] << " ";
+		}
+		cout << endl;
+	}
+	void max_Heap()
+	{
+		int index = size - 1;
+		while (index > 0)
+		{
+			if (heapArr[parent(index)] < heapArr[index])
+			{
+				int parentVal = heapArr[parent(index)];
+				int childVal = heapArr[index];
+				swap(parentVal, childVal);
+				heapArr[parent(index)] = parentVal;
+				heapArr[index] = childVal;
+				index = parent(index);
+			}
+			else
+			{
+				index = parent(index);
+			}
+		}
+		cout << "Max Heap : ";
+		for (int i = 0; i < size; i++)
+		{
+			cout << heapArr[i] << " ";
+		}
+		cout << endl;
+	}
+	void display()
+	{
+		cout << "Heap Tree : ";
+		for (int i = 0; i < size; i++)
+		{
+			cout << heapArr[i] << " ";
+		}
+		cout << endl;
+	}
+};
