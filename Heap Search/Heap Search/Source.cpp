b06b5abbefb634e57259502bc36bd2c23e7080b1
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-06-20 13:38:24 +0500

    Update Source.cpp

--- Heap Search/Heap Search/Source.cpp
+++ Heap Search/Heap Search/Source.cpp
@@ -3,13 +3,13 @@
 int main()
 {
 	HeapTree obj(5);
-	obj.insert(5);
+	obj.insert(4);
+	obj.insert(6);
 	obj.insert(10);
-	obj.insert(3);
-	obj.insert(8);
-	obj.insert(1);
+	obj.insert(9);
+	obj.insert(2);
 	obj.display();
-	//obj.minHeap();
-	obj.maxHeap();
+	obj.minHeap();
+	//obj.maxHeap();
 	return 0;
 }
