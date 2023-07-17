070bb349d7d38fb2b65f69e406936d6add420d9e
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-07-17 08:08:46 +0500

    practice

---
+++ Final Practice Questions/ConsoleApplication2/ConsoleApplication2/Source.cpp
@@ -0,0 +1,16 @@
+#include"BST.h"
+
+int main()
+{
+	BST obj;
+	obj.insert(8);
+	obj.insert(3);
+	obj.insert(9);
+	obj.insert(1);
+	obj.insert(6);
+	obj.insert(2);
+	obj.display();
+
+
+	return 0;
+}
